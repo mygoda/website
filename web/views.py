7789e9fba47c88b10f6a5de2545657c93b17570f
xutao10 <xutao10@baidu.com>
2017-11-26 13:46:46 +0800

    add view

--- web/views.py
+++ web/views.py
@@ -8,21 +8,6 @@ from web.models import Banner, Shop, Type
 # Create your views here.
 
 
-def async_shops(req):
-    """
-        异步获取店面详情
-    :param req:
-    :return:
-    """
-    types = Type.objects.all()
-    type_shops = [type.to_json() for type in types]
-    banners = Banner.objects.all()
-    result = {
-        "banners": [banner.to_json() for banner in banners],
-        "types": type_shops
-    }
-
-
 def index(req):
     """
         首页
@@ -37,6 +22,23 @@ def index(req):
         "types": type_shops
     }
     return render_to_response("index.html", result, RequestContext(req))
+
+
+def query(req):
+    """
+        商店列表
+    :param req:
+    :return:
+    """
+    type_id = req.GET.get("type_id")
+    shops = Shop.objects.filter(type_id=type_id)
+    types = Type.objects.all()
+    result = {
+        "shops": shops,
+        "type_id": type_id,
+        "types": types
+    }
+    return render_to_response("shops.html", result, RequestContext(req))
 
 
 def detail(req, shop_id):
